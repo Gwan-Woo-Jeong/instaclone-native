6029a6494d0def9721e4f7be0398bb207bfe444d
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2021-10-17 12:41:37 +0900

    me profile css finished

--- screens/Me.tsx
+++ screens/Me.tsx
@@ -1,11 +1,22 @@
 import { gql, useQuery } from "@apollo/client";
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  ListRenderItem,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import styled from "styled-components/native";
 import useMe from "../hooks/useMe";
 import { MeProps } from "../propTypes";
 import { PHOTO_FRAGMENT } from "./fragments";
-import { seeProfile } from "./__generated__/seeProfile";
+import {
+  seeProfile,
+  seeProfile_seeProfile_photos,
+} from "./__generated__/seeProfile";
 
 const SEE_PROFILE_QUERY = gql`
   query seeProfile($username: String!) {
@@ -36,36 +47,62 @@ const Header = styled.View`
   flex: 0.2;
   flex-direction: row;
   margin-top: 20px;
+  align-items: center;
+  justify-content: center;
 `;
-const Grid = styled.View``;
+const Grid = styled.View`
+  margin-top: 20px;
+  flex: 0.8;
+`;
 
-const ProfileContainer = styled.View``;
-const FollowContainer = styled.View`
+const ProfileWrapper = styled.View`
   flex-direction: row;
 `;
-const NameContainer = styled.View``;
-const BioContainer = styled.View``;
+const ProfileContainer = styled.View`
+  justify-content: center;
+`;
+const FollowContainer = styled.View`
+  flex-direction: row;
+  margin-bottom: 8px;
+`;
 
 const Avatar = styled.Image`
-  height: 135px;
-  width: 135px;
+  height: 120px;
+  width: 120px;
   border-radius: 500px;
-  margin-left: 20px;
+  margin-right: 35px;
 `;
 const UsernameText = styled.Text`
   color: white;
-  margin-bottom: 3px;
+  margin-bottom: 15px;
+  font-size: 20px;
+  font-weight: 800;
 `;
 
 const NameText = styled.Text`
   color: white;
+  font-size: 16px;
+  font-weight: 600;
+  margin-bottom: 7px;
 `;
 const FollowText = styled.Text`
   color: white;
+  margin-right: 10px;
+  font-size: 13px;
 `;
+
+const FollowNumber = styled(FollowText)`
+  font-weight: 700;
+`;
+
 const BioText = styled.Text`
   color: white;
+  font-size: 14px;
 `;
+
+const NoPhoto = styled.View``;
+const NoPhotoText = styled.Text``;
+
 const EditProfileBtn = styled.TouchableOpacity``;
 const EditProfileText = styled.Text``;
 
@@ -74,33 +111,65 @@ function Me({ navigation }: MeProps) {
   const { data } = useQuery<seeProfile>(SEE_PROFILE_QUERY, {
     variables: { username: meData?.me?.username },
   });
+  const numColumns = 3;
+  const { width } = useWindowDimensions();
+
   useEffect(() => {
     navigation.setOptions({
       title: meData?.me?.username,
       headerTitleAlign: "center",
     });
   }, []);
+  const renderItem: ListRenderItem<seeProfile_seeProfile_photos | null> = ({
+    item: photo,
+  }) => (
+    <TouchableOpacity
+      style={{ width: width / numColumns, height: 100, padding: 2 }}
+    >
+      <Image
+        style={{ width: "100%", height: "100%" }}
+        source={{ uri: photo!.file }}
+      />
+    </TouchableOpacity>
+  );
   return (
     <Container>
       <Header>
         <Avatar source={{ uri: data?.seeProfile?.avatar! }} />
-        <ProfileContainer>
-          <UsernameText>{data?.seeProfile?.username}</UsernameText>
-          <FollowContainer>
-            <FollowText>
-              {data?.seeProfile?.totalFollowers} Followers
-            </FollowText>
-            <FollowText>
-              {data?.seeProfile?.totalFollowing} Followings
-            </FollowText>
-          </FollowContainer>
-          <NameText>
-            {data?.seeProfile?.firstName + " " + data?.seeProfile?.lastName}
-          </NameText>
-          <BioText>{data?.seeProfile?.bio}</BioText>
-        </ProfileContainer>
+        <ProfileWrapper>
+          <ProfileContainer>
+            <UsernameText>{data?.seeProfile?.username}</UsernameText>
+            <FollowContainer>
+              <FollowText>
+                <FollowNumber>{data?.seeProfile?.totalFollowers}</FollowNumber>{" "}
+                Followers
+              </FollowText>
+              <FollowText>
+                <FollowNumber>{data?.seeProfile?.totalFollowing}</FollowNumber>{" "}
+                Followings
+              </FollowText>
+            </FollowContainer>
+            <NameText>
+              {data?.seeProfile?.firstName + " " + data?.seeProfile?.lastName}
+            </NameText>
+            <BioText>{data?.seeProfile?.bio}</BioText>
+          </ProfileContainer>
+        </ProfileWrapper>
       </Header>
-      <Grid></Grid>
+      <Grid>
+        {data?.seeProfile?.photos ? (
+          <FlatList
+            data={data?.seeProfile?.photos}
+            keyExtractor={(photo) => "" + photo?.id}
+            renderItem={renderItem}
+            numColumns={numColumns}
+          />
+        ) : (
+          <NoPhoto>
+            <NoPhotoText>There is no photo yet</NoPhotoText>
+          </NoPhoto>
+        )}
+      </Grid>
     </Container>
   );
 }
